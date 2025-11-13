0e327557b54aa9f5ac528f2a05c564cde941ed77
Omar El-Sawy <ok.elsawy@gmail.com>
2025-11-13 10:17:33 -0500

    Fixed cace being misnamed, and fixed libparse not working

--- shell.nix
+++ shell.nix
@@ -220,8 +220,8 @@ in
       fi
       if [ -n "$VIRTUAL_ENV" ]; then
           echo "Installing Python packages from requirements.txt..."
-          python -m pip install --upgrade pip setuptools wheel maturin
-          python -m pip install -r "$PROJECT_ROOT/requirements.txt"
+          python -m pip install --upgrade pip setuptools wheel maturin cace
+          python -m pip install --no-build-isolation -r "$PROJECT_ROOT/requirements.txt"
 
           for pkg in analog/library/dep_library/gmid analog/library/dep_library/UWASIC-ALG; do
               if [ -d "$PROJECT_ROOT/$pkg" ]; then
