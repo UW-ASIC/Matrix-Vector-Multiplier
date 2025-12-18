3c795a2cbbfbfeef8f62c06234b5fbf4008909d3
davvdf <david88gao@gmail.com>
2025-12-17 21:48:23 -0500

    sv

---
+++ analog/schematics/testbenches/digital_stim/DAC_stim.sv
@@ -0,0 +1,11 @@
+// Digital input for DAC testbench
+module DAC_stim #(parameter WIDTH = 8)(
+    input logic clk,
+    input logic rst_n,
+    output logic [WIDTH-1:0] dac_out
+);
+
+// Simple counter to generate digital values for DAC
+
+
+endmodule
