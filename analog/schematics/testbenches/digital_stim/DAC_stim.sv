daf6726ff6c8e510603038d76f8a672e54adbcc6
davvdf <david88gao@gmail.com>
2025-12-21 16:09:10 -0500

    fixed xschemrc and generated boilerplate for stimulus symbols, kind of broken atm

--- analog/schematics/testbenches/digital_stim/DAC_stim.sv
+++ analog/schematics/testbenches/digital_stim/DAC_stim.sv
@@ -1,16 +1,12 @@
 `timescale 1ns/1ns
-module DAC_stim();
+module DAC_stim(
+    input logic clk,
+    input logic rst_n,
+    output logic [7:0] dac_out
+);
     // Parameters
     parameter DAC_WIDTH  = 8;
     parameter CLK_PERIOD = 15.151515;
 
-    // Signals
-    logic clk;
-    logic rst_n;
-    logic [DAC_WIDTH-1:0] dac_data;
-    logic dac_out;
-
-    initial begin
-        forever #(CLK_PERIOD/2) clk = ~clk;
-    end
+    // Instantiate DUT
 endmodule
