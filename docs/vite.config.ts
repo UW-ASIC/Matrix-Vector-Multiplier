2bf8af2fe723f01b0baf342b4208afac39af8c09
Omar El-Sawy <ok.elsawy@gmail.com>
2026-01-07 18:03:14 -0500

    fixed repo name

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TinyTapeout_Flows/', // Replace with your EXACT repository name
+  base: "/Matrix-Vector-Multiplier/", // Replace with your EXACT repository name
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
-  }
-})
+  },
+});
+
